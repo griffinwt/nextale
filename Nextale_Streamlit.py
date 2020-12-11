651e1d93ca9f078bd6927d7ea19e1bc6cbefb279
griffinwt <69866169+griffinwt@users.noreply.github.com>
2020-12-10 19:31:14 -0700

    changed format

--- Nextale_Streamlit.py
+++ Nextale_Streamlit.py
@@ -15,7 +15,7 @@ st.title('Use the drop-down menu on the left to find your next adventure...')
 
 page = st.sidebar.selectbox(
     'Select-A-Page',
-    ('Overview', 'Books', 'Movies', 'Video Games')
+    ('Overview', 'Books', 'Movies', 'Video Games', 'How It Works')
 )
 
 #recommender function
@@ -78,34 +78,6 @@ if page == 'Overview':
 Recommendation systems are ubiquitous in today's attention-driven online environment, whether used by an online retailer to recommend similar products or a media application to prompt binge-watching. These systems use a combination of well known and proprietary techniques to engage customers with customized recommendations based on their previous preference. In this project, I created a relatively simple version of one of these recommenders using customer reviews between 1995-2015 provided by Amazon. I specifically looked at products in the categories of books, movies, and video games. To reflect the narrative element in each of these products, I titled my project "Nextale". It is a tool to assist the user in finding their "next tale" based on something they enjoyed previously.
 
 ''')
-    st.subheader('How It Works')
-    st.write('''
-This model finds the similarity between products by measuring the distance between their vectors created from review grades by customers. Here is an example of what the initial reviewer by product matrix looked like on a small scale (with artificial ratings):
-    ''')
-
-    st.table(pd.read_pickle('./compressed/sample_pivot.pkl'))
-
-    st.write('''
-We can see here that the vector created from the ratings of Batman Returns is [3,4,1] and the vector of Frozen will be [5,1,5]. These vectors are like lines pointing out from the same origin point. The more similar the contents are, the more similar the direction in which they point. The cosine distance (angle) between these lines will be a measure between 0 and 1 where 0 (no distance) means they are identical and 1 means no relationship. Here's an image that represents those vectors:
-    ''')
-
-    st.image('./images/sample_vectors.png', use_column_width=True)
-
-    st.write('''
-We see that Frozen and Mulan are on very similar trajectories and the angle between them is small. Contrastingly, The Shining is pointing in quite a different direction, with Batman Returns somewhere in between. Here is what that distance looks like numerically - the number in each cell represents the pairwise distance between items.    
-    ''')
-
-    st.table(pd.read_pickle('./compressed/sample_rec.pkl'))
-
-    st.write('''
-As we might expect, the distance between each film and itself is 0 (along the diagonal)! By selecting a product and sorting the numerical contents of that product's column from least to greatest, we can predict similarity between items. For example:
-    ''')
-
-    st.table(pd.read_pickle('./compressed/fro_df.pkl'))
-
-    st.write('''
-Obviously Frozen's best match is with itself... after that we would recommend Mulan, then Batman Returns, then The Shining, which intuitively is probably the order we would expect. My recommender works just like this, but in a few more than 3 dimensions!
-    ''')
 
 elif page =='Books':
     st.subheader('Books')
@@ -195,3 +167,34 @@ Try it out for yourself:
     st.write('''
     *in order to expedite search results and save memory, this application works off of a truncated version of the full recommender; for each product, the 100 most similar products are stored in memory. This means that theoretically, if a user's "exclusion" search term is too broad, less than 10 similar products may be available which would compromise the resulting printout. While possible, this error has never been observed in any tests of the application to date.
     ''')
+
+elif page == 'How It Works':
+
+    st.subheader('How It Works')
+        st.write('''
+    This model finds the similarity between products by measuring the angle between their vectors created from review grades by customers. Here is an example of what the initial reviewer by product matrix looked like on a small scale (with artificial ratings):
+        ''')
+
+        st.table(pd.read_pickle('./compressed/sample_pivot.pkl'))
+
+        st.write('''
+    We can see here that the vector created from the ratings of Batman Returns is [3,4,1] and the vector of Frozen will be [5,1,5]. These vectors are like lines pointing out from the same origin point. The more similar the contents are, the more similar the direction in which they point. The cosine distance (angle) between these lines will be a measure between 0 and 1 where 0 (no distance) means they are identical and 1 means no relationship. Here's an image that represents those vectors:
+        ''')
+
+        st.image('./images/sample_vectors.png', use_column_width=True)
+
+        st.write('''
+    We see that Frozen and Mulan are on very similar trajectories and the angle between them is small. Contrastingly, The Shining is pointing in quite a different direction, with Batman Returns somewhere in between. Here is what that distance looks like numerically - the number in each cell represents the pairwise distance between items.    
+        ''')
+
+        st.table(pd.read_pickle('./compressed/sample_rec.pkl'))
+
+        st.write('''
+    As we might expect, the distance between each film and itself is 0 (along the diagonal)! By selecting a product and sorting the numerical contents of that product's column from least to greatest, we can predict similarity between items. For example:
+        ''')
+
+        st.table(pd.read_pickle('./compressed/fro_df.pkl'))
+
+        st.write('''
+    Obviously Frozen's best match is with itself... after that we would recommend Mulan, then Batman Returns, then The Shining, which intuitively is probably the order we would expect. My recommender works just like this, but with millions of reviewers instead of just 3!
+        ''')
